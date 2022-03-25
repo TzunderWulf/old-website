36219d148ea9f52fc35f69eb7a8d0eb2a94d1f4f
TzunderWulf <svandenbos@tzunder.nl>
2022-03-25 02:03:35 +0100

    Two new sections, contact methods

--- js/main.js
+++ js/main.js
@@ -5,7 +5,12 @@ menu.addEventListener("click", hamburgerMenu);
 
 let fullMenu = document.getElementById("full-menu");
 let hiddenMenu = document.getElementById("hidden-menu");
+let navButtons = document.getElementsByClassName("nav-link");
 const sticky = fullMenu.offsetTop;
+
+for (let navButton of navButtons) {
+  navButton.addEventListener("click", closeMenu)
+}
 
 function hamburgerMenu(e) {
   let menu = e.target;
@@ -17,12 +22,16 @@ function hamburgerMenu(e) {
     hiddenMenu.classList.toggle('hidden')
     fullMenu.classList.toggle('background');
   } else {
-    menu.classList.toggle('bi-x-lg')
-    menu.classList.toggle('hamburger-closed');
-    menu.classList.toggle('bi-list');
-    hiddenMenu.classList.toggle('hidden')
-    fullMenu.classList.toggle('background');
+    closeMenu();
   }
+}
+
+function closeMenu() {
+  menu.classList.toggle('bi-x-lg')
+  menu.classList.toggle('hamburger-closed');
+  menu.classList.toggle('bi-list');
+  hiddenMenu.classList.toggle('hidden')
+  fullMenu.classList.toggle('background');
 }
 
 function makeMenuSticky() {
